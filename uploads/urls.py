7ef8675405cb47030ab7ef940de9d5d270041086
nataliadoczekalska <natalia.doczekalska@op.pl>
2019-11-14 17:21:57 +0100

    pierwszy commit

--- uploads/urls.py
+++ uploads/urls.py
@@ -1,12 +1,28 @@
-from django.conf.urls import url
-from django.contrib import admin
+"""jedzonko URL Configuration
 
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
 from uploads.core import views
 
-
 urlpatterns = [
-    url('^$', views.home, name='home'),
-    url('data_analysis/', views.data_analysis, name='data_analysis'),
-    url('about/', views.about, name='about'),
-    url('contact/', views.contact, name='contact'),
+    path('admin/', admin.site.urls),
+    path('', views.stronaglowna, name='stronaglowna'),
+    path('pizza/', views.pizza, name='pizza'),
+    path('burgery/', views.burgery, name='burgery'),
+    path('kebab/', views.kebab, name='kebab'),
+    path('tajskie/', views.tajskie, name='tajskie'),
+    path('wege/', views.wege, name='wege'),
 ]
